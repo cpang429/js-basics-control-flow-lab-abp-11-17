f6a8919ca4dfd4a417cef0bc6e03ce8639e1360f
Chris Pang <chris_pang429@outlook.com>
2017-11-08 20:37:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+Function scuberGreetingForFeet(distance) {
+  if (distance <= 400) {
+    console.log('This one is on me!')
+  } else if (distance > 2500) {
+    console.log('No can do');
+  } else {
+    console.log('I will gladly take your 30 bucks');
+  }
+}
